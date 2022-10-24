9930da7275a776e4622fdbb6d7af228cf4c241a9
facundogiovine <facundogiovine11@gmail.com>
2022-10-24 00:30:42 -0300

    Cambios en Search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import logo from "../images/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="TRAINERS" className="logo" />
+      <div className="user">
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BMTI3MDc4NzUyMV5BMl5BanBnXkFtZTcwMTQyMTc5MQ@@._V1_UY264_CR16,0,178,264_AL_.jpg"
+          alt=""
+          className="pfp"
+        />
+        <span>John Doe</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" />
+      </div>
+      <div className="userChat">
+        <img src="https://m.media-amazon.com/images/M/MV5BMTI3MDc4NzUyMV5BMl5BanBnXkFtZTcwMTQyMTc5MQ@@._V1_UY264_CR16,0,178,264_AL_.jpg" />
+      </div>
+      <div className="userChatInfo">
+        <span>Sum Ting Wong</span>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
