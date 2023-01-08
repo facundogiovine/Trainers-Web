824fff6abf21c86bbbd80c382810f91dd41b3998
facundogiovine <facundogiovine11@gmail.com>
2023-01-08 18:00:26 -0300

    EventHandler Entrenador

--- src/model/Entrenador.jsx
+++ src/model/Entrenador.jsx
@@ -1,15 +1,15 @@
 export class Entrenador{
-    constructor(){
-        this.id = id,
-        this.descripcion = descripcion,
-        this.calificacion = calificacion,
-        this.experiencia = experiencia,
-        this.latitud = latitud,
-        this.longitud = longitud,
-        this.activo = activo,
-        this.nombres = nombres,
-        this.apelidos = apellidos,
-        this.nombreMostrado = nombreMostrado,
+    constructor(id, descripcion, calificacion, experiencia, latitud, longitud, activo, nombres, apellidos, nombreMostrado, fechaNacimiento){
+        this.id = id;
+        this.descripcion = descripcion;
+        this.calificacion = calificacion;
+        this.experiencia = experiencia;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.activo = activo;
+        this.nombres = nombres;
+        this.apelidos = apellidos;
+        this.nombreMostrado = nombreMostrado;
         this.fechaNacimiento = fechaNacimiento;
     }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,10 +5,11 @@ import "../components/FontAwesomeIcons";
 import logo from "../images/logo.png";
 import { FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react";
 import { useFormik } from "formik";
+import { Entrenador } from "../model/Entrenador";
 import * as Yup from "yup"
 const Login = () => {
   const formik = useFormik({
-    initialValues: {email: "", contrasena: ""},
+    initialValues: { email: "", contrasena: "" },
     validationSchema: Yup.object({
       email: Yup.string().required("Ingrese un correo electronico."),
       contrasena: Yup.string().required("Ingrese su contrasena."),
@@ -17,9 +18,28 @@ const Login = () => {
       // alert(JSON.stringify(values, null, 2));
       // console.log(values);
       actions.resetForm();
-      fetch("http://localhost:8080/api/v1/auth/autenticar/entrenador", {method:"POST", body:JSON.stringify(values, null, 2), headers: {"Content-Type": "application/json"}}).then(response => response.json()).then(json => console.log(json));
+      let response;
+      fetch("http://localhost:8080/api/v1/auth/autenticar/entrenador", { method: "POST", body: JSON.stringify(values, null, 2), headers: { "Content-Type": "application/json" } }).then(response => response.json()).then(response => responseHandler(response)).then(entrenador => console.log(entrenador));
+      // if (response) {
+      //   let entrenador = responseHandler(response);
+
+      //   console.log(entrenador);
+      // }
     },
   });
+
+  const responseHandler = (response) => {
+    let entrenador;
+    if (response.autenticado) {
+      let id = response.id;
+
+      fetch(`http://localhost:8080/api/v1/entrenador/entrenador/${id}`).then((response) => response.json()).then(r =>{ entrenador = new Entrenador(r.id, r.descripcion, r.calificacion, r.experiencia, r.latitud, r.longitud, r.activo, r.nombres, r.apellidos, r.nombreMostrado, r.fechaNacimiento); return entrenador}
+      ).then(e => { entrenador = e });
+
+    }
+    console.log(entrenador);
+    return entrenador;
+  }
   return (
     <div className="formContainer">
       <div className="formWrapper">
@@ -27,11 +47,11 @@ const Login = () => {
 
         <form onSubmit={formik.handleSubmit}  >
           <FormControl isInvalid={formik.errors.email && formik.touched.email}>
-            <input name="email" placeholder="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email}   />
+            <input name="email" placeholder="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
             <FormErrorMessage color={"red"}>{formik.errors.email}</FormErrorMessage>
           </FormControl>
-          
-          <FormControl isInvalid={formik.errors.contrasena && formik.touched.contrasena }>
+
+          <FormControl isInvalid={formik.errors.contrasena && formik.touched.contrasena}>
             <input name="contrasena" type="password" placeholder="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.contrasena} />
             <FormErrorMessage color={"red"}> {formik.errors.contrasena}</FormErrorMessage>
           </FormControl>
