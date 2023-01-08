2c90df82fe7c1116b62e67f1a98f25d73462018d
facundogiovine <facundogiovine11@gmail.com>
2023-01-08 17:09:47 -0300

    Autentificado
    Muchos Modules, rework de la pagina de Login, autentificacion funcionando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './components/FontAwesomeIcons'
 import './style.scss';
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import Register from "./pages/Register"
+import Views from "./pages/Views"
 import logo from "./images/logo.png"
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/">
-
-    //       <Route path="login" element={<Login />} />
-    //       <Route path="register" element={<Register />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-
-
-    <Home />
+    <Views />
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,45 @@
 import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, Form } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../components/FontAwesomeIcons";
 import logo from "../images/logo.png";
+import { FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup"
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {email: "", contrasena: ""},
+    validationSchema: Yup.object({
+      email: Yup.string().required("Ingrese un correo electronico."),
+      contrasena: Yup.string().required("Ingrese su contrasena."),
+    }),
+    onSubmit: (values, actions) => {
+      // alert(JSON.stringify(values, null, 2));
+      // console.log(values);
+      actions.resetForm();
+      fetch("http://localhost:8080/api/v1/auth/autenticar/entrenador", {method:"POST", body:JSON.stringify(values, null, 2), headers: {"Content-Type": "application/json"}}).then(response => response.json()).then(json => console.log(json));
+    },
+  });
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <img src={logo} className="logo"></img>
-        <form>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button>Log in!</button>
-          {/* {err && <span>Something went wrong</span>} */}
+
+        <form onSubmit={formik.handleSubmit}  >
+          <FormControl isInvalid={formik.errors.email && formik.touched.email}>
+            <input name="email" placeholder="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email}   />
+            <FormErrorMessage color={"red"}>{formik.errors.email}</FormErrorMessage>
+          </FormControl>
+          
+          <FormControl isInvalid={formik.errors.contrasena && formik.touched.contrasena }>
+            <input name="contrasena" type="password" placeholder="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.contrasena} />
+            <FormErrorMessage color={"red"}> {formik.errors.contrasena}</FormErrorMessage>
+          </FormControl>
+
+          <button type="submit">Log in!</button>
+
         </form>
-        <p>
-          {/* Don't have an account? <Link to="/register">Register</Link> */}
-        </p>
+
       </div>
     </div>
   );
