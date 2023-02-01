67df025c1df4ae5e814f6ebfd304fd37e68c4030
facundogiovine <facundogiovine11@gmail.com>
2023-02-01 13:52:26 -0300

    Date and sides
    Date and sideees

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import useIntl from 'react-intl';
+import { obtenerEntrenador } from "../utils/utils";
 
 const Message = ({ mensaje }) => {
+  console.log(mensaje);
+  const date = new Date(mensaje.fecha);
+  const options = { day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit' };
+  const fechaFormatted = new Intl.DateTimeFormat('es-PY', options).format(date);
+
+  let entrenador = obtenerEntrenador();
+  const isOwner = mensaje.senderId === entrenador.id;
+  const messageOwnership = `message ${isOwner ? 'Owner' : 'NotOwner'}`;
+
   return (
-    <div className="message Owner">
+    <div className={messageOwnership}>
       <div className="messageInfo">
         <img
           src="https://m.media-amazon.com/images/M/MV5BMTI3MDc4NzUyMV5BMl5BanBnXkFtZTcwMTQyMTc5MQ@@._V1_UY264_CR16,0,178,264_AL_.jpg"
           alt=""
         />
-        <span>just now</span>
+        <span>{fechaFormatted}</span>
       </div>
       <div className="messageContent">
         <p>{mensaje.contenido}</p>
-        {/* <img
-          src="https://m.media-amazon.com/images/I/31yqYjsAi6L._AC_SY1000_.jpg"
-          alt=""
-        /> */}
       </div>
     </div>
   );
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,8 @@
-import Cookies from "js-cookie";
-
 export const obtenerEntrenador = () => {
     return JSON.parse(Cookies.get('entrenador'));
 };
 
 export const obtenerFechaIso = () => {
     let date = new Date();
-    return `${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDay().toString().padStart(2, '0')}T${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}-03:00`;
+    return `${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}T${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}-03:00`;
 }
