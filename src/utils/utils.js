9869b68f3aa368f9723176fc60648c4056ea1829
Guillermo Miranda <gdmn3000@gmail.com>
2023-02-01 23:36:08 -0300

    Manejar cookies vacías cuando la sesión está cerrada

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,14 @@
 import Cookies from "js-cookie";
 
 export const obtenerEntrenador = () => {
-    return JSON.parse(Cookies.get('entrenador'));
+  let entrenador = Cookies.get('entrenador');
+  if (entrenador) {
+    return JSON.parse(entrenador);
+  }
+  return null;
 };
 
 export const obtenerFechaIso = () => {
-    let date = new Date();
-    return `${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}T${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}-03:00`;
+  let date = new Date();
+  return `${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}T${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}-03:00`;
 }
