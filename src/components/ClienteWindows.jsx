d48cb9788b8fea3f364163a03dc73a741ea5eb2f
facundogiovine <facundogiovine11@gmail.com>
2023-02-03 15:04:16 -0300

    Se crea componente

---
+++ src/components/ClienteWindows.jsx
@@ -0,0 +1,11 @@
+const ClienteWindows = () => {
+    return (
+        <div className="clienteWindows">
+            <div className="container">
+                
+            </div>
+        </div>
+    );
+};
+
+export default ClienteWindows;
