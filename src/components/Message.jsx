6cf7f31992b4a8ca36b6f50dc696b19b910c6782
facundogiovine <facundogiovine11@gmail.com>
2023-02-07 18:29:44 -0300

    Fecha correcta, hora formateada segun timezone

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,10 +3,20 @@ import useIntl from 'react-intl';
 import { obtenerEntrenador } from "../utils/utils";
 
 const Message = ({ mensaje }) => {
+  let formattedDate = mensaje.fecha;
   let entrenador = obtenerEntrenador();
   const date = new Date(mensaje.fecha);
-  const options = { day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit' };
-  const fechaFormatted = new Intl.DateTimeFormat('es-PY', options).format(date);
+  if (isFinite(date)) {
+    
+    formattedDate = new Intl.DateTimeFormat(undefined, {
+      timeZone: 'UTC',
+      day: "2-digit",
+      month: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit"
+    }).format(new Date(mensaje.fecha));
+
+  }
   return (
     <div className={`message ${String(mensaje.senderId) === String(entrenador.id) ? 'Owner' : 'NotOwner'}`}>
       <div className="messageInfo">
@@ -14,7 +24,7 @@ const Message = ({ mensaje }) => {
           src="https://m.media-amazon.com/images/M/MV5BMTI3MDc4NzUyMV5BMl5BanBnXkFtZTcwMTQyMTc5MQ@@._V1_UY264_CR16,0,178,264_AL_.jpg"
           alt=""
         />
-        <span>{fechaFormatted}</span>
+        <span>{formattedDate}</span>
       </div>
       <div className="messageContent">
         <p>{mensaje.contenido}</p>
